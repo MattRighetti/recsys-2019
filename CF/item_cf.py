e0eee3d0aa364062fa05fdf00c36017f2d4941e6
Mattia Righetti <matt95.righetti@gmail.com>
2019-11-12 23:18:06 +0100

    Initial ItemCFRecommender

--- CF/item_cf.py
+++ CF/item_cf.py
@@ -1,11 +1,51 @@
 import numpy as np
-from Algorithms.Notebooks_utils import Compute_Similarity_Python
+from scipy.sparse import csr_matrix
+
+from Algorithms.Notebooks_utils.Compute_Similarity_Python import Compute_Similarity_Python
 
 class ItemBasedCollaborativeFiltering(object):
     """
     ItemBasedCollaborativeFiltering
     """
-    def __init__(self):
-        print("ItemBasedCF Created")
 
-    
+    def __init__(self, URM, topK=50, shrink=100):
+        self.URM = URM
+        self.topK = topK
+        self.shrink = shrink
+
+    def fit(self, normalize=True, similarity='cosine'):
+        similarity_object = Compute_Similarity_Python(self.URM, self.topK, self.shrink, normalize=normalize, similarity=similarity)
+
+        self.W_sparse = similarity_object.compute_similarity()
+
+    def recommend(self, user_id, at=None, exclude_seen=True):
+        # Compute the scores using the dot product
+        user_profile = self.URM[user_id]
+        scores = user_profile.dot(self.W_sparse).toarray().ravel()
+
+        if exclude_seen:
+            scores = self.filter_seen(user_id, scores)
+
+        # Rank items
+        ranking = scores.argsort()[::-1]
+
+        return ranking[:at]
+
+    def filter_seen(self, user_id, scores):
+        """
+        Function that removes items already seen by the user
+        :param user_id: User ID corresponding to each row index of the URM
+        :param scores: Every rating of the corresponding User ID
+        :return: Scored without already seen items
+        """
+        target_row = user_id
+        start_pos = self.URM.indptr[target_row]
+        end_pos = self.URM.indptr[target_row + 1]
+
+        user_profile = self.URM.indices[start_pos:end_pos]
+
+        scores[user_profile] = -np.inf
+
+        return scores
+
+recommender = ItemBasedCollaborativeFiltering()
