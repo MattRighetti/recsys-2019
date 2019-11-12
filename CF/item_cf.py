34b6acf54749d6417c1b83502229565dd7efa2d0
Mattia Righetti <matt95.righetti@gmail.com>
2019-11-12 01:18:17 +0100

    Initial recommender systems setup

---
+++ CF/item_cf.py
@@ -0,0 +1,9 @@
+import numpy as np
+from Algorithms.Notebooks_utils import Compute_Similarity_Python
+
+class ItemBasedCollaborativeFiltering(object):
+    """
+    ItemBasedCollaborativeFiltering
+    """
+    def __init__(self):
+        print("ItemBasedCF Created")
---
+++ CF/user_cf.py
@@ -0,0 +1,9 @@
+import numpy as np
+from Algorithms.Notebooks_utils import Compute_Similarity_Python
+
+class UserBasedCollaborativeFiltering(object):
+    """
+    UserBasedCollaborativeFiltering recommender system
+    """
+    def __init__(self):
+        print("UserBasedCF created!")
