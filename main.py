5f566f78c4539233c5b31fc17a2b6ffa5b94b319
Mattia Righetti <matt95.righetti@gmail.com>
2019-11-22 19:01:03 +0100

    Cleaned code for CFs

--- main.py
+++ main.py
@@ -2,37 +2,35 @@ from Algorithms.Notebooks_utils.evaluation_function import evaluate_MAP_target_u
 from Utils.Toolkit import TestGen, DataReader
 from Utils.Toolkit import TestSplit
 from CF.item_cf import ItemBasedCollaborativeFiltering
+from CF.user_cf import UserBasedCollaborativeFiltering
 from Utils.OutputWriter import write_output
 
 import numpy as np
 
-dr = DataReader()
-URM_all_CSR = dr.URM_CSR()
-target_users = dr.targetUsersList
-lou = TestGen(URM_all_CSR, TestSplit.LEAVE_ONE_OUT)
-URM_train = lou.URM_train
-URM_test = lou.URM_test
-topK_collection = [10, 15, 22, 35, 50, 70, 90, 120, 150]
-shrink_collection = [5, 10, 20, 30, 40, 50, 80, 120, 200]
+URM_all_CSR = DataReader().URM_CSR()
+target_users = DataReader().targetUsersList
+
+lou_matrices = TestGen(URM_all_CSR, TestSplit.LEAVE_ONE_OUT)
+
+URM_train = lou_matrices.URM_train
+URM_test = lou_matrices.URM_test
+
+topK_collection = np.arange(5, 11, 5)
+shrink_collection = np.arange(20, 26, 1)
 max_map = 0
 max_topK = 0
 max_shrink = 0
 
 for topK in topK_collection:
     for shrink in shrink_collection:
-        map_collection = []
 
-        itemCF1 = ItemBasedCollaborativeFiltering(URM_train=URM_train, topK=topK, shrink=shrink)
-        itemCF1.fit()
+        itemCF1 = ItemBasedCollaborativeFiltering(topK=topK, shrink=shrink)
+        itemCF1.fit(URM_train)
         map_elem = evaluate_MAP_target_users(URM_test, itemCF1, target_users)
-        map_collection.append(map_elem)
         print(map_elem)
 
-        map_avg = np.average(map_collection)
-        print("Average MAP " + str(map_avg))
-
-        if map_avg > max_map:
-            max_map = map_avg
+        if map_elem > max_map:
+            max_map = map_elem
             max_topK = topK
             max_shrink = shrink
 
@@ -45,6 +43,6 @@ print("Map " + str(max_map))
 
 URM_final = URM_train + URM_test
 
-itemCF1 = ItemBasedCollaborativeFiltering(URM_final, max_topK, max_shrink)
-itemCF1.fit()
+itemCF1 = ItemBasedCollaborativeFiltering(max_topK, max_shrink)
+itemCF1.fit(URM_final)
 write_output(itemCF1, target_users)
