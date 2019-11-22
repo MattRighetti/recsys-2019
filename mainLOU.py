7693ed8f2a215fd32681ec07b3b1906f61ce0c0d
Mattia Righetti <matt95.righetti@gmail.com>
2019-11-22 18:10:16 +0100

    Deleted useless mains

--- mainLOU.py
+++
@@ -1,8 +0,0 @@
-from Utils.Toolkit import TestGen, DataReader
-from Utils.Toolkit import TestSplit
-
-
-dr = DataReader()
-URM_all_CSR = dr.URM_CSR()
-target_users = dr.targetUsersList
-k_fold = TestGen(URM_all_CSR, TestSplit.LEAVE_ONE_OUT, k=3)
