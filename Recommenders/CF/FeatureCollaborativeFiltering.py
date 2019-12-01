d419587cd775ced07e53ccfc5501e9f10db6fdb7
Mattia Righetti <matt95.righetti@gmail.com>
2019-12-01 04:38:03 +0100

    Implemented FeatureCollaborativeFiltering

---
+++ Recommenders/CF/FeatureCollaborativeFiltering.py
@@ -0,0 +1,55 @@
+from Utils.Toolkit import get_URM_TFIDF, normalize_matrix, get_data, generate_SM_user_feature_matrix
+from Algorithms.Notebooks_utils.evaluation_function import evaluate_MAP_target_users, evaluate_MAP
+from Algorithms.Base.Similarity.Cython.Compute_Similarity_Cython import Compute_Similarity_Cython
+from Recommenders.BaseRecommender import BaseRecommender
+import scipy.sparse as sps
+import numpy as np
+
+
+class FeatureCollaborativeFiltering(BaseRecommender):
+
+    RECOMMENDER_NAME = "FeatureCollaborativeFiltering"
+
+    def __init__(self, topK, shrink):
+        super().__init__()
+        self.ICM = None
+        self.topK = topK
+        self.shrink = shrink
+        self.SM_feature = None
+        self.SM_user_feature = None
+        self.RM = None
+
+    def get_similarity_matrix(self, similarity='tanimoto'):
+        similarity_object = Compute_Similarity_Cython(self.ICM,
+                                                      self.shrink,
+                                                      self.topK,
+                                                      normalize = True,
+                                                      tversky_alpha = 1.0,
+                                                      tversky_beta = 1.0,
+                                                      similarity = similarity)
+        return similarity_object.compute_similarity()
+
+    def fit(self, ICM, URM_train):
+        self.ICM = ICM.tocsr()
+        self.SM_user_feature = generate_SM_user_feature_matrix(URM_train, self.ICM)
+        self.SM_feature = self.get_similarity_matrix()
+        self.RM = self.SM_user_feature.dot(self.SM_feature)
+
+    def recommend(self, user_id, at=10, exclude_seen=False):
+        expected_ratings = self.get_expected_ratings(user_id)
+        recommended_items = np.flip(np.argsort(expected_ratings), 0)
+
+        return recommended_items[:at]
+
+    def get_expected_ratings(self, user_id):
+        expected_recommendations = self.RM[user_id].todense()
+        return np.squeeze(np.asarray(expected_recommendations))
+
+    def get_features_ratings(self):
+        data = []
+
+        for i in range(self.RM.shape[0]):
+            recommended_features = np.flip(np.argsort(self.RM[i].toarray().ravel()), 0)
+            data.append(recommended_features)
+
+        return sps.csr_matrix(data)
